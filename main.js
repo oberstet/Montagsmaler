5e860a6e950b5a3c684a4706e27e6d47a1d778bc
FHAux <katrin.riedelbauch@hs-augsburg.de>
2014-04-16 10:47:23 +0200

    Create main.js
    In der main.js sind unsere publish-/subscribe- und onevent-Funktionen für die Echtzeitverbindung definiert.

---
+++ main.js
@@ -0,0 +1,105 @@
+// Package definieren
+this.$cc$ = {game : {}};
+
+(function($cc$, window)
+{
+  function main()
+  {
+    // Zeichenklasse initialisieren
+    var zeichnen = new $cc$.game.zeichnen(this);
+
+    AUTOBAHN_DEBUG = true;
+
+    this.connection = new autobahn.Connection(
+    {
+      url : 'ws://127.0.0.1:8080/ws',
+      realm : 'realm1'
+    });
+
+    // Wird ausgeführt wenn die Verbindung hergestellt ist
+    this.connection.onopen = function(session)
+    {
+
+      console.log("session established!");
+
+      // Die Eventhändler
+      //
+      function onmousedown(mousePos)
+      {
+        console.log(mousePos);
+        zeichnen.begin(mousePos);
+      };
+
+      function onmousemove(mousePos)
+      {
+        console.log(mousePos);
+        console.log(zeichnen);
+        console.log(zeichnen.moveOn);
+        zeichnen.moveOn(mousePos);
+      };
+
+      // Für die verschiedenen Themen registrieren
+      //
+      session.subscribe('de.copycat.mousedown', onmousedown).then(
+          function(subscription)
+          {
+            console.log("ok, subscribed with ID " + subscription.id);
+          }, function(error)
+          {
+            console.log(error);
+          });
+
+      session.subscribe('de.copycat.mousemove', onmousemove).then(
+          function(subscription)
+          {
+            console.log("ok, subscribed with ID " + subscription.id);
+          }, function(error)
+          {
+            console.log(error);
+          });
+    };
+
+    // Wird ausgeführt wenn die Verbindung abbricht
+    this.connection.onclose = function(reason, details)
+    {
+      console.log("connection lost", reason);
+    };
+
+    // Öffnet die Verbindung
+    this.connection.open();
+   
+  }
+
+  main.prototype =
+  {
+    // Unsere Publishmethoden  
+    publishmousedown : function(mousePos)
+    {
+      if (this.connection.session)
+      {
+        this.connection.session.publish("de.copycat.mousedown",
+        [mousePos]);
+      } else
+      {
+        console.log("cannot publish: no session");
+      }
+    },
+
+    publishmousemove : function(mousePos)
+    {
+      if (this.connection.session)
+      {
+        this.connection.session.publish("de.copycat.mousemove",
+        [mousePos]);
+      } else
+      {
+        console.log("cannot publish: no session");
+      }
+    },
+
+    
+  };
+
+  window.onload = function() {new main();};
+
+}(this.$cc$, this.window));
